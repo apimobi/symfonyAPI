65b746e77bb7f8e7159d96668ee7b7a9ff450847
Vincent <vincent.montuclard@brandalley.com>
2016-08-18 12:42:12 +0200

    app no bootable

--- web/js/app_main_3.js
+++ web/js/app_main_3.js
@@ -1,7 +1,7 @@
 $( document ).ready(function(){
   $(".button-collapse").sideNav();
   $('.modal-trigger').leanModal();
-  showModal = true;
+  
   if(showModal)
   {
       $('#modal1').openModal();
